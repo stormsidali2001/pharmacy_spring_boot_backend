6fa9ded0ee96e1ea5388990e5809031f5c11c884
shAtud <sidalihouda.computerscience@gmail.com>
2022-06-11 22:12:38 +0100

    ...

---
+++ src/main/java/com/signinsignup/basic_signin_signup/models/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.signinsignup.basic_signin_signup.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order,Long>{
+   
+}
--- src/main/java/com/signinsignup/basic_signin_signup/orders/OrdersService.java
+++ src/main/java/com/signinsignup/basic_signin_signup/orders/OrdersService.java
@@ -1,18 +1,40 @@
 package com.signinsignup.basic_signin_signup.orders;
 
+import java.util.ArrayList;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.signinsignup.basic_signin_signup.models.Order;
+import com.signinsignup.basic_signin_signup.models.OrderRepository;
+import com.signinsignup.basic_signin_signup.models.Product;
+import com.signinsignup.basic_signin_signup.models.ProductRepository;
+import com.signinsignup.basic_signin_signup.models.User;
+import com.signinsignup.basic_signin_signup.models.enums.OrderStatus;
 import com.signinsignup.basic_signin_signup.orders.dto.OrderProductDTO;
 
 @Service
 public class OrdersService {
 
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private ProductRepository productRepository;
+
     public String createOrder(Set<OrderProductDTO> products) {
-        products.forEach(pr->{
-            
-        });
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user  = (User) auth.getPrincipal();
+      
+
+        // ArrayList<Order> orders = new ArrayList<>();
+        // products.forEach(pr->{
+        //     Product product = productRepository.getById(pr.getProductId());
+        //     Order order = new Order(client, product, pr.getQuantity(), OrderStatus.WAIT);
+          
+        // });
         return null;
     }
     
